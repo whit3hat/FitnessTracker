084ecef178acebfa09481828c39c58c276ed6688
Spencer Wood <spencer.wood@me.com>
2020-01-24 21:22:39 -0600

    created row and col for date / time and wrote sudo code in js file

---
+++ script.js
@@ -0,0 +1,15 @@
+//get todays date
+
+//get lift user will do
+
+//provide list of preloaded workouts
+
+//store new lift user enters
+
+//store reps and sets user does
+
+//store weight user lifts
+
+//graph weights user did over time 1 month / 3 months / 6 months
+
+//provide options for pre defined workouts
